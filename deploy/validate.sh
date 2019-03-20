36d7e9db6dd6019edb95238eb3593c23e5f0c08a
xiaqiulei <xiaqiulei@126.com>
2019-03-20 17:55:54 +0800

    update deploy script

--- deploy/validate.sh
+++ deploy/validate.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 url="http://127.0.0.1:9090/heartbeat";
-echo $url
-while [ true ]
+echo ${url}
+
+for ((i=1; i<=120; i ++))
 do
     sleep 1
-    HTTP_CODE=`curl -G -m 10 -o /dev/null -s -w %{http_code} $url`
-    echo "http code: ${HTTP_CODE}"
-    if [ ${HTTP_CODE} -eq 200 ]
+    HTTP_CODE=`curl -G -m 10 -o /dev/null -s -w %{http_code} ${url}`
+    echo "No.$i --> http code: ${HTTP_CODE}"
+    if [[ ${HTTP_CODE} -eq 200 ]]
     then
         echo "server start success..."
         exit 0
