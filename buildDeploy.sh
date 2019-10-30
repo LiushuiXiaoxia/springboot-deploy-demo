8386707ecb7903828030f9a4fd0712a28b24876a
xiaqiulei <xiaqiulei@126.com>
2019-10-30 14:04:55 +0800

    build sh

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm deploy/*.jar
-./gradlew clean build && ./gradlew --stop && cp build/libs/* deploy/
+./gradlew clean assemble && cp build/libs/* deploy/
--- deploy/start.sh
+++ deploy/start.sh
@@ -17,6 +17,7 @@ pid=$(ps -ef | grep java | grep $jarfile | awk '{print $2}')
 #create log dir
 mkdir -p $PRGDIR/log/
 
+mkdir ../log
 nohup java -jar $jarfile -Dfile.encoding=UTF-8 --spring.profiles.active=prod > $PRGDIR/../log/start.log 2>&1 &
 pid=$(ps -ef | grep java | grep $jarfile | awk '{print $2}')
 echo "INFO: $jarfile is running! pid=$pid"
